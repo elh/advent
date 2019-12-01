bc2ffd60c6e128ff305269b7a155566205e16173
Eugene Huang <eugene@goforward.com>
2019-12-01 15:26:32 -0800

    solved day 1

---
+++ 1/1.1.clj
@@ -0,0 +1,15 @@
+(when (not= (count *command-line-args*) 1)
+  (do
+    (println "expects exactly one command line arg. EXIT")
+    (. System exit 1)))
+
+; Fuel required to launch a given module is based on its mass. Specifically, to find the fuel
+; required for a module, take its mass, divide by three, round down, and subtract 2.
+(defn fuel [mass] 
+  (- (int (Math/floor (/ mass 3))) 2))
+
+(def inputs (clojure.string/split-lines (slurp (first *command-line-args*))))
+
+(println (reduce + (map fuel (map read-string inputs))))
+
+; CORRECT: 3479429
