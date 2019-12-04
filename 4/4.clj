506fbd3375f1d1e7bac748bd350800acaa3184b5
Eugene Huang <eugene@goforward.com>
2019-12-03 22:33:00 -0800

    solved day 4.2

--- 4/4.clj
+++ 4/4.clj
@@ -1,3 +1,4 @@
+; copied this from SO
 (defn digits [n]
   (for [d  (str n)]
     (- (byte d) 48)))
@@ -13,11 +14,31 @@
                   (map digits
                        (range (+ a 1) b)))))
 
+(defn pt2perms [a b]
+  (filter (fn [perm]
+            (not= -1 (.indexOf (get
+                                (reduce (fn [data digit]
+                                          {
+                                           :prior digit
+                                           :runs (if
+                                                  (= (get data :prior) digit)
+                                                   (conj (pop (get data :runs)) (+ 1 (last (get data :runs))))
+                                                   (conj (get data :runs) 0)
+                                                   )
+                                           })
+                                        {:prior -1 :runs [0]}
+                                        perm)
+                                :runs)
+                               1)))
+          (filter #(= (sort %) %)
+                  (map digits
+                       (range (+ a 1) b)))))
+
 ; p1
 (println (time (count (validperms 108457 562041))))
 ; CORRECT: 2779
 
 ; p2
-; (println (time (p2 inputs)))
-; CORRECT: ???
+(println (time (count (pt2perms 108457 562041))))
+; CORRECT: 1972
 
